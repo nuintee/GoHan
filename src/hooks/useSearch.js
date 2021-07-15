00d127241759769cc62dbafe06359c5960082d1d
Sho <staff@iMacA.local>
2021-07-15 17:34:16 +0900

    feat: measuring process time for search function

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -24,11 +24,15 @@ const useSearch = () => {
 
     const SearchData = (position) => {
         randomInt = Math.floor(100 * Math.random())// Random <Int>
-
+        const startTime = performance.now()
+        const timeout = setTimeout(`alert('5秒以上かかります。')`,5000)
         fetch(`https://food-server.glitch.me/?lat=${position.latitude}&lng=${position.longitude}&index=${randomInt}`)
         //fetch(`http://localhost:3000/?lat=${position.latitude}&lng=${position.longitude}&int=${randomInt}`)
         .then(res => res.json())
         .then(doc => {
+            const endTime = performance.now()
+            console.log( (endTime - startTime) / 1000 )
+            clearTimeout(timeout)
             PickData(doc)
             window.location.href = '/#/result'
         })
