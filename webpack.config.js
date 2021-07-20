e826e151970f337cca9c8c4ebc3ed5c68a717200
Sho <staff@iMacA.local>
2021-07-21 00:33:18 +0900

    deleted all comments from the code issue#1

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env','@babel/preset-react',
-                        // {
-                        //     "targets": {
-                        //         "node": "current"
-                        //     }
-                        // }
-                    ]
+                        presets: ['@babel/preset-env','@babel/preset-react']
                     }
                 }
             },
